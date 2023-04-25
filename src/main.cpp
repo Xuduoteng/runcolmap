6380f1bec5309c50bda11567d3cb86f3948918a4
xuduoteng <1820331297@qq.com>
2023-04-25 14:25:41 +0800

    runcolmap

--- src/main.cpp
+++ src/main.cpp
@@ -191,7 +191,7 @@ int runcolmap_with_align()
 
 		checkFolder(output_root / "imgs");
 
-#pragma omp parallel for
+		#pragma omp parallel for
 		for (int i_img = 0; i_img < img_paths.size(); ++i_img)
 		{
 			const auto in_img_path = (fs::path(img_path) / img_paths[i_img]).string();
@@ -306,10 +306,10 @@ int runcolmap_with_align()
 
 			if (isHighQuality)
 			{
-				//sift_extraction_options.estimate_affine_shape = true;
+				sift_extraction_options.estimate_affine_shape = true;
 				sift_extraction_options.domain_size_pooling = true;
 				sift_extraction_options.max_num_features = 40000;
-				//sift_extraction_options.num_threads = 6;
+				sift_extraction_options.num_threads = 6;
 			}
 
 			SiftFeatureExtractor sift(image_reader_options, sift_extraction_options);
@@ -369,7 +369,7 @@ int runcolmap_with_align()
 
 		// recon option
 		incremental_mapper_options.multiple_models = isMultipleModels;
-		incremental_mapper_options.min_num_matches = 100; // 15 for more points +100
+		incremental_mapper_options.min_num_matches = 15; // 15 for more points +100
 
 		// ba option
 		if (isGreaterBa)
@@ -809,17 +809,17 @@ int runcolmap()
 // std::string scene_src_path, std::string scene_output_path
 int recon_scene()
 {
-	const fs::path input_root("H:/data/temp/scene0000_00");
+	const fs::path input_root("H:/data/scene0000_00/data_20");
 	const fs::path output_root("H:/data/scannet_colmap/scene0000_00");
 
 	const std::string img_path = (input_root / "color").string();			// Path of the input image
 	const fs::path pose_folder_path = input_root / "pose";					// c2w pose
-	const fs::path intr_txt_path = input_root / "intrinsics_color.txt";		// c2w pose
-	const std::string db_path = (input_root / "database.db").string();		// Canbe blank
+	const fs::path intr_txt_path = input_root / "intrinsics_color.txt";		// 
+	const std::string db_path = (output_root / "database.db").string();		// Canbe blank
 
 	// Some config option
 	// Recon option
-	bool isHighQuality = false;
+	bool isHighQuality = true;
 	bool isMultipleModels = false;
 	bool isUseGpu = true;
 	bool isSingleCamera = false;
@@ -862,10 +862,10 @@ int recon_scene()
 
 			if (isHighQuality)
 			{
-				//sift_extraction_options.estimate_affine_shape = true;
+				sift_extraction_options.estimate_affine_shape = true;
 				sift_extraction_options.domain_size_pooling = true;
 				sift_extraction_options.max_num_features = 40000;
-				//sift_extraction_options.num_threads = 6;
+				sift_extraction_options.num_threads = 6;
 			}
 
 			SiftFeatureExtractor sift(image_reader_options, sift_extraction_options);
@@ -900,7 +900,7 @@ int recon_scene()
 			Eigen::Matrix<double, 4, 4> intr_matrix = ReadMatrixFromFile(intr_txt_path.string());
 			const double focal_length_in_pixel = intr_matrix(0, 0);
 
-			//set cameras prior(focal length)
+			// set cameras prior(focal length)
 			std::vector<Camera> cameras = database.ReadAllCameras();
 			for (auto& camera : cameras) {
 				camera.SetFocalLength(focal_length_in_pixel);
@@ -935,13 +935,261 @@ int recon_scene()
 		// fix the image pose
 		incremental_mapper_options.fix_existing_images = true;
 		// Which intrinsic parameters to optimize during the reconstruction.
-		incremental_mapper_options.ba_refine_focal_length = false;
-		incremental_mapper_options.ba_refine_extra_params = false;
-		incremental_mapper_options.ba_refine_principal_point = true;
+		/*incremental_mapper_options.ba_refine_focal_length = true;
+		incremental_mapper_options.ba_refine_extra_params = true;
+		incremental_mapper_options.ba_refine_principal_point = false;*/
 
 		// recon option
 		incremental_mapper_options.multiple_models = isMultipleModels;
-		incremental_mapper_options.min_num_matches = 100; // 15 for more points +100
+		incremental_mapper_options.min_num_matches = 5; // 15 for more points +100
+
+		// ba option
+		if (isGreaterBa)
+		{
+			incremental_mapper_options.ba_local_max_num_iterations = 40;
+			incremental_mapper_options.ba_local_max_refinements = 3;
+			incremental_mapper_options.ba_global_max_num_iterations = 100;
+			incremental_mapper_options.ba_global_use_pba = true;
+		}
+
+		IncrementalMapperController mapper(&incremental_mapper_options, img_path, db_path, &reconstruction_manager);
+		mapper.Start();
+		mapper.Wait();
+		mapper.Stop();
+
+		LOG(INFO) << boost::format("Reconstruction_manager.Size():%d") % (int)reconstruction_manager.Size();
+		// for each recon model
+		for (int i = 0; i < reconstruction_manager.Size(); i++)
+		{
+			LOG(INFO) << boost::format("========== Sparse Reconstruction %d/%d is Processing ==========") % (i + 1)
+				% (int)reconstruction_manager.Size();
+
+			Reconstruction& reconstruction = reconstruction_manager.Get(i);
+
+			// Print some RegImageIds info
+			for (const image_t i_img : reconstruction.RegImageIds()) {
+				if (!reconstruction.IsImageRegistered(i_img))
+					continue;
+				LOG(INFO) << boost::format("Img %d: %d; ; Matched points: %d; Valid points: %d")
+					% i_img
+					% database.NumKeypointsForImage(i_img)
+					% reconstruction.Image(i_img).NumCorrespondences()
+					% reconstruction.Image(i_img).NumPoints3D();
+			}
+			LOG(INFO) << boost::format("%d registred images; %d 3D points")
+				% reconstruction.NumRegImages()
+				% reconstruction.NumPoints3D();
+
+			// Export src result
+			std::string outfile("sparse" + std::to_string(i));
+			safeCheckFolder(output_root / outfile);
+			reconstruction.WriteBinary((output_root / outfile).string());
+			reconstruction.WriteText((output_root / outfile).string());
+			//reconstruction.ExportPLY((output_root / outfile / "sparse.ply").string());
+			ExportPLYwithNormal(reconstruction, (output_root / outfile / "sparse.ply").string());
+			LOG(INFO) << boost::format("Reconstruction %d : Save to %s") % (i + 1) % (output_root / outfile);
+		}
+	}
+
+	// dense Reconstruction
+	LOG(INFO) << "===== Beginning Dense Reconstruction =====";
+	using namespace mvs;
+	assert(reconstruction_manager.Size() != 0);
+	for (int i = 0; i < reconstruction_manager.Size(); i++)
+	{
+		LOG(INFO) << boost::format("============= Dense Reconstruction %d/%d Begin =============\n") % (i + 1)
+			% (int)reconstruction_manager.Size();
+		std::string outfile_dense("dense" + std::to_string(i));
+		std::string dense_path = (output_root / outfile_dense).string();
+		std::string fused_path = (output_root / outfile_dense / "fused.ply").string();
+		safeCheckFolder(output_root / outfile_dense);
+
+		Reconstruction& reconstruction = reconstruction_manager.Get(i);
+
+		// Undistort images and export undistorted cameras, as required by the mvs::PatchMatchController class.
+		LOG(INFO) << "===== Beginning Images Undistortion =====";
+		UndistortCameraOptions undistortion_options;
+		undistortion_options.max_image_size = max_image_size;
+		COLMAPUndistorter undistorter(undistortion_options, reconstruction, img_path, dense_path);
+		undistorter.Start();
+		undistorter.Wait();
+		undistorter.Stop();
+
+		// MVS
+		LOG(INFO) << "===== Beginning Patch Match =====";
+		PatchMatchOptions patchMatchOptions;
+		patchMatchOptions.max_image_size = max_image_size;
+		patchMatchOptions.gpu_index = "0";
+		// patchMatchOptions.filter_min_triangulation_angle = 1.0f;
+
+		PatchMatchController patch_match_controller(patchMatchOptions, dense_path, "COLMAP", "");
+		patch_match_controller.Start();
+		patch_match_controller.Wait();
+		patch_match_controller.Stop();
+
+		// Stereo fusion
+		LOG(INFO) << "===== Beginning Stereo Fusion =====";
+		StereoFusionOptions stereoFusionOptions;
+		stereoFusionOptions.max_image_size = max_image_size;
+		const int num_reg_images = reconstruction.NumRegImages();
+		stereoFusionOptions.min_num_pixels = std::min(num_reg_images + 1, stereoFusionOptions.min_num_pixels);
+		StereoFusion fuser(stereoFusionOptions, dense_path, "COLMAP", "", "geometric");
+		fuser.Start();
+		fuser.Wait();
+		fuser.Stop();
+
+		std::cout << "Writing output: " << fused_path << std::endl;
+		WriteBinaryPlyPoints(fused_path, fuser.GetFusedPoints());
+		WritePointsVisibility(fused_path + ".vis", fuser.GetFusedPointsVisibility());
+		LOG(INFO) << boost::format("Dense Reconstruction %d : Save to %s") % (i + 1) % (fused_path);
+
+		// Surface meshing
+		LOG(INFO) << "===== Beginning Surface Meshing =====";
+		std::string meshing_path = (output_root / outfile_dense / "meshed-poisson.ply").string();
+		PoissonMeshingOptions poissonMeshingOptions;
+		poissonMeshingOptions.num_threads = 4;
+		poissonMeshingOptions.trim = 1;
+		PoissonMeshing(poissonMeshingOptions, fused_path, meshing_path);
+		LOG(INFO) << boost::format("PoissonMesh %d : Save to %s") % (i + 1) % (meshing_path);
+	}
+}
+
+// std::string scene_src_path, std::string scene_output_path
+int recon_etu()
+{
+	const fs::path input_root("H:/Data/ETU/delivery_area");
+	const fs::path output_root("H:/Data/ETU/delivery_area/colmap_recon");
+
+	const std::string img_path = (input_root / "images" / "dslr_images_undistorted").string();	// Path of the input image
+	//const fs::path pose_folder_path = input_root / "pose";									// c2w pose
+	//const fs::path intr_txt_path = input_root / "dslr_calibration_undistorted" / "cameras.txt";	//
+	const std::string db_path = (output_root / "database.db").string();							// Canbe blank
+
+	// Some config option
+	// Recon option
+	bool isHighQuality = false;
+	bool isMultipleModels = false;
+	bool isUseGpu = true;
+	bool isSingleCamera = false;
+	bool isGreaterBa = true;
+	std::int16_t max_image_size = 6208;
+
+	//Colmap running
+	LOG(INFO) << "===== Colmap Running =====";
+	ReconstructionManager reconstruction_manager;
+	// sparse recon exists, loading
+	if (fs::exists(output_root / "sparse0")) {
+		LOG(INFO) << "===== Existing Sparse Reconstruction, Dense Reconstruction Running =====";
+		std::int16_t i = -1;
+		// maybe sparse0,1,2.. exists
+		while (fs::exists(output_root / ("sparse" + std::to_string(++i)))) {
+			reconstruction_manager.Read((output_root / ("sparse" + std::to_string(i))).string());
+		}
+	}
+	// sparse recon
+	else {
+		LOG(INFO) << "===== Beginning Sparse Reconstruction =====";
+		Database database(db_path);
+
+		/*************************************** SIFT ***************************************/
+		// SIFT config
+		{
+			ImageReaderOptions image_reader_options;
+			SiftExtractionOptions sift_extraction_options;
+
+			image_reader_options.database_path = db_path;
+			image_reader_options.image_path = img_path;
+			image_reader_options.camera_model = "SIMPLE_RADIAL";
+			image_reader_options.single_camera = isSingleCamera;
+			//image_reader_options.single_camera_per_folder   = true;
+
+			sift_extraction_options.use_gpu = isUseGpu;
+			//sift_extraction_options.upright = true;
+			sift_extraction_options.gpu_index = "0";
+			sift_extraction_options.max_image_size = max_image_size;
+
+			if (isHighQuality)
+			{
+				sift_extraction_options.estimate_affine_shape = true;
+				sift_extraction_options.domain_size_pooling = true;
+				sift_extraction_options.max_num_features = 40000;
+				sift_extraction_options.num_threads = 6;
+			}
+
+			SiftFeatureExtractor sift(image_reader_options, sift_extraction_options);
+			sift.Start();
+			sift.Wait();
+			sift.Stop();
+		}
+
+		// feature matching
+		{
+			SiftMatchingOptions sift_matching_options;
+			ExhaustiveMatchingOptions exhaustive_matching_options;
+			sift_matching_options.use_gpu = isUseGpu;
+			sift_matching_options.gpu_index = "0";
+
+			if (isHighQuality)
+			{
+				sift_matching_options.guided_matching = true;
+				//sift_matching_options.planar_scene    = true;
+				//sift_matching_options.max_num_matches = 43000;
+			}
+			ExhaustiveFeatureMatcher matcher(exhaustive_matching_options, sift_matching_options, db_path);
+			matcher.Start();
+			matcher.Wait();
+			matcher.Stop();
+		}
+
+		/********************************** update database *********************************/
+		// set the pior
+		{
+			// parse the intr_txt and get the focal length
+			/*Eigen::Matrix<double, 4, 4> intr_matrix = ReadMatrixFromFile(intr_txt_path.string());
+			const double focal_length_in_pixel = intr_matrix(0, 0);*/
+
+			// set cameras prior(focal length)
+			std::vector<Camera> cameras = database.ReadAllCameras();
+			for (auto& camera : cameras) {
+				camera.SetFocalLength(3408.7);
+				camera.SetPriorFocalLength(true);
+				database.UpdateCamera(camera);
+			}
+
+			// for each image in database, we set and fixed its pose
+			/*std::vector<Image> images = database.ReadAllImages();
+			for (auto& image : images) {
+				std::string txtName = image.Name();
+				size_t pos = txtName.find_last_of(".");
+				if (pos != std::string::npos) {
+					txtName.replace(pos + 1, txtName.length() - pos - 1, "txt");
+				}
+
+				Eigen::Matrix<double, 4, 4> extr_matrix = ReadMatrixFromFile((pose_folder_path / txtName).string()).inverse();
+				Eigen::Quaterniond q(extr_matrix.block<3, 3>(0, 0));
+
+				image.SetQvec(Eigen::Vector4d(q.w(), q.x(), q.y(), q.z()));
+				image.SetTvec(extr_matrix.col(3).head<3>());
+
+				image.SetQvecPrior(Eigen::Vector4d(q.w(), q.x(), q.y(), q.z()));
+				image.SetTvecPrior(extr_matrix.col(3).head<3>());
+				database.UpdateImage(image);
+			}*/
+		}
+
+		/*************************************** SFM ***************************************/
+		IncrementalMapperOptions incremental_mapper_options;
+
+		// fix the image pose
+		incremental_mapper_options.fix_existing_images = true;
+		// Which intrinsic parameters to optimize during the reconstruction.
+		/*incremental_mapper_options.ba_refine_focal_length = true;
+		incremental_mapper_options.ba_refine_extra_params = true;
+		incremental_mapper_options.ba_refine_principal_point = false;*/
+
+		// recon option
+		incremental_mapper_options.multiple_models = isMultipleModels;
+		incremental_mapper_options.min_num_matches = 15; // 15 for more points +100
 
 		// ba option
 		if (isGreaterBa)
